202d074cd1f62939f4db565fe99e5ba29be03c3d
harroyo0610 <humberto-_-lzc@hotmail.com>
2017-02-28 17:30:18 -0600

    Suma

---
+++ calculadora.js
@@ -0,0 +1,21 @@
+suma = 0;
+
+
+$( function() {
+    // drag = $( "#lista" ).children().draggable(hello());
+    $( "#lista" ).children().draggable({
+  drag: function( event, ui ) {
+    $(this).append($(this).clone().one());
+    //$(this).clone().append("<h1>rve</h1>");
+  }
+
+});
+    $( "#cardSlots" ).droppable({
+      drop: function( event, ui ) {
+        val = ui.draggable.find("li").attr("value")
+        suma += parseInt(val);
+        $("#total_sum").text(suma);
+      }
+    });
+  } );
+
